2d8cd3b9d8e53738d34a513718e09cba7e9dce24
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-11-19 14:05:17 -0600

    prompt finalized

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,57 @@
 from fastapi import FastAPI
-
+import json
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
 app = FastAPI()
 
-# load_dotenv()
+load_dotenv()
 
-# api_key = os.getenv('GOOGLE_API_KEY')
+api_key = os.getenv('GOOGLE_API_KEY')
 
-# genai.configure(api_key = api_key  )
 
-# model = genai.GenerativeModel("gemini-1.5-flash")
-# prompt = """Generate a easy level question for the following job description:
-# Title: Front-end Developer
-# Position: Intern
-# Skills: ReactJs, JavaScript, UI, NextJs
-# Also provide a good answer for this. 
-# """
-# response = model.generate_content([
-#     prompt
-# ])
+def query(prompt):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    genai.configure(api_key = api_key  )
+    structured_propmt = f"""
+        Generate an new set of technical interview question following this exact JSON structure, without any markdown formatting or array wrapping:
+        {{
+            "easy": {{
+                "Question": "your_question_here",
+                "Answer": "your_answer_here"
+            }},
+            "medium": {{
+                "Question": "your_question_here",
+                "Answer": "your_answer_here"
+            }},
+            "hard": {{
+                "Question": "your_question_here",
+                "Answer": "your_answer_here"
+            }}
+        }}
 
-# print(response.text)
+        Additional requirements:
+        1. Provide the response as pure JSON without code blocks or markdown
+        2. Give relevant questions that might appear in actual technical interview for the respective positions 
+        3. Ensure all answers are properly escaped for JSON
+        4. Do not provide any Examples inside any answers. 
+        5. Make questions and answers relevant to the following context:
+
+        {prompt}
+
+        
+        """
+    response = model.generate_content([
+        structured_propmt
+    ])
+    return response.text #response is a json so need to be converted to text for string
+
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    prompt = 'title: Front end developer, position: intern '
+    response = query(prompt)
+    print(response)
+    json_obj = json.loads(response)
+    return {"message": json_obj}
