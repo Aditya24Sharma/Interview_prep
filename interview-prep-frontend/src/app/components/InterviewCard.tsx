96d7fb8d674e47b2883e63a1a2f7f856e7f0a5fa
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-12-20 23:02:07 -0500

    retry feedback btn

--- interview-prep-frontend/src/app/components/InterviewCard.tsx
+++ interview-prep-frontend/src/app/components/InterviewCard.tsx
@@ -1,4 +1,5 @@
-import {Calendar} from 'lucide-react'
+import {Calendar, RotateCcw, MessageCircle } from 'lucide-react'
+import Link from 'next/link'
 import { Star } from 'lucide-react'
 
 interface Interview{
@@ -19,6 +20,20 @@ export default function InterviewCard({interview}: {interview: Interview}){
             <Calendar className="mr-2 text-red-800" size={32} />
             <span className='text-xl'>{interview.date}</span>
           </div>
+          <div className="flex justify-between mt-6">
+          {/* <Link href={`/interview-session?id=${interview.id}`} passHref> */}
+            <button className="flex text-xl items-center justify-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-300">
+              <RotateCcw size={24} className="mr-2" />
+              Retry
+            </button>
+          {/* </Link> */}
+          {/* <Link href={`/feedback-session?id=${interview.id}`} passHref> */}
+            <button className="flex text-xl items-center justify-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-300">
+              <MessageCircle size={24} className="mr-2" />
+              Feedback
+            </button>
+          {/* </Link> */}
+        </div>
         </div>
       )
 }
